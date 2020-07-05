c02b5b4a542520f8324e0ce19be2fd2ac6af64a4
alessandro-silva <alessandro.rds03@gmail.com>
2020-07-05 18:32:59 -0400

    :barber: add signup

--- gobarber-web/src/App.tsx
+++ gobarber-web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import SignIn from './pages/SignIn';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStryle from './styles/global';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStryle />
   </>
 );
